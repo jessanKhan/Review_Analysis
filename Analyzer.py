4cb59dbc4d0c7eb975c45b34ccfc85315ba73740
jessanKhan <39450312+jessanKhan@users.noreply.github.com>
2020-10-07 02:10:33 +0600

    analyzer methode add

--- Analyzer.py
+++ Analyzer.py
@@ -17,6 +17,7 @@ root=Tk()
 files=[]
 all_reviews=[]
 data_train=[]
+data_test=[]
 #Functions
 def addtrainfile():
     global data_train
@@ -42,10 +43,32 @@ def addtestfile():
     data_test=pd.read_csv(filename2, delimiter='\t', header=None)
     data_test.columns= ['Reviews_text', 'Review_Class']
     print(data_test)
+    return data_test
 
 
 
 
+
+def clean_test_text(df):
+    all_test = list()
+    lines = df["Reviews_text"].values.tolist()
+   
+    for text in lines:
+        text = text.lower()
+        pattern = re.compile('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
+        text = pattern.sub('', text)
+        text = re.sub(r"[,.\"!@#$%^&*(){}?/;`~:<>+=-]", "", text)
+        tokens = word_tokenize(text)
+        table = str.maketrans('', '', string.punctuation)
+        stripped = [w.translate(table) for w in tokens]
+        words = [word for word in stripped if word.isalpha()]
+        stop_words = set(stopwords.words("english"))
+        stop_words.discard("not")
+        PS = PorterStemmer()
+        words = [PS.stem(w) for w in words if not w in stop_words]
+        words = ' '.join(words)
+        all_test.append(words)
+    return all_test
 
 def clean_text(df):
     all_reviews = list()
@@ -69,31 +92,33 @@ def clean_text(df):
     return all_reviews
 
 def prediction():
-    data_imdb=pd.read_csv('imdb_labelled.txt', delimiter='\t', header=None)
-    data_imdb.columns= ['Reviews_text', 'Review_Class']
+    # data_imdb=pd.read_csv('imdb_labelled.txt', delimiter='\t', header=None)
+    # data_imdb.columns= ['Reviews_text', 'Review_Class']
 
-    data_amazon = pd.read_csv("amazon_cells_labelled.txt", delimiter='\t', header=None)
-    data_amazon.columns = ["Reviews_text", "Review_Class"]
+    # data_amazon = pd.read_csv("amazon_cells_labelled.txt", delimiter='\t', header=None)
+    # data_amazon.columns = ["Reviews_text", "Review_Class"]
 
-    data_yelp = pd.read_csv("yelp_labelled.txt", delimiter='\t', header=None)
-    data_yelp.columns = ["Reviews_text", "Review_Class"]
+    # data_yelp = pd.read_csv("yelp_labelled.txt", delimiter='\t', header=None)
+    # data_yelp.columns = ["Reviews_text", "Review_Class"]
 
-    data = pd.concat([data_imdb, data_amazon, data_yelp])
-    # data=data_train
+    # data = pd.concat([data_imdb, data_amazon, data_yelp])
+    data=data_train
     all_reviews = clean_text(data)
+    all_test=clean_test_text(data)
     TV = TfidfVectorizer(min_df=3)   
-    X = TV.fit_transform(all_reviews).toarray()
-    y = data[["Review_Class"]]
+    X_train = TV.fit_transform(all_reviews).toarray()
+    X_test=TV.fit_transform(all_test).toarray()
+    y_train = data[["Review_Class"]]
     # print(np.shape(X))
     # print(np.shape(y))
-    X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.20,random_state=4)
+    # X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.20,random_state=4)
     # print(X_test.shape)
     classifier = svm.SVC(kernel='linear',gamma='auto', C=2)
     classifier.fit(X_train,y_train)
     y_predict = classifier.predict(X_test)
-    reoprtprint= classification_report(y_test,y_predict)
-    print(classification_report(y_test,y_predict))
-    print(classification_report(y_test,y_predict))
+    # reoprtprint= classification_report(y_test,y_predict)
+    # print(classification_report(y_test,y_predict))
+    # print(classification_report(y_test,y_predict))
     print(y_predict)
     Positives=np.count_nonzero(y_predict==1)
     Negative=np.count_nonzero(y_predict==0)
@@ -101,7 +126,7 @@ def prediction():
         print("Kharap")
     else:
         print('Valo')
-    return reoprtprint
+    
 
 
 
